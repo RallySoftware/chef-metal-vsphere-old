3e0a910e1b571a1abdee44aec982d9f75d92937e
Brian Dupras <brian@duprasville.com>
2014-07-05 07:26:12 -0600

    removed junk file

--- test/destroy_all.rb
+++
@@ -1,8 +0,0 @@
-require 'chef_metal'
-
-Chef::Log.warn "Destroy machines [#{search(:node, '*:*').map { |n| n.name }}]"
-
-machine_batch do
-  machines search(:node, '*:*').map { |n| n.name }
-  action :destroy
-end
