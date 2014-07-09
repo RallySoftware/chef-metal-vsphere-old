5ebee68c800a57909511b3309b854b9cf5f562f2
Brian Dupras <bdupras@rallydev.com>
2014-07-09 07:53:15 -0600

    comment out some unfinished stuff

--- test/chef-repo/cookbooks/test/recipes/simple.rb
+++ test/chef-repo/cookbooks/test/recipes/simple.rb
@@ -4,9 +4,9 @@ machine 'mario' do
   tag 'itsa_me'
   converge true
 
-  add_machine_options(bootstrap_options: {
-      annotation: 'Annotation from recipe.  Woot.'
-      })
+  # add_machine_options(bootstrap_options: {
+  #     annotation: 'Annotation from recipe.  Woot.'
+  #     })
 end
 
 machine_file '/tmp/hello_world' do
